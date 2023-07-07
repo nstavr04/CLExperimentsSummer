577be08da2ea312efcaec191333587e5530a66d3
nstavr04 <nstavr04@ucy.ac.cy>
2023-07-07 13:42:15 +0300

    .

--- newestOfflineExperiments/experiments.py
+++ newestOfflineExperiments/experiments.py
@@ -179,9 +179,9 @@ class Experiments:
             #### End of the above ####
 
             # Store the representations of the new samples in the replay buffer
-            # cl_model.storeRepresentationsNativeRehearsal(train_x, train_y, i+1)
+            cl_model.storeRepresentationsNativeRehearsal(train_x, train_y, i+1)
 
-            cl_model.BRS(features, train_x, train_y, i+1)
+            # cl_model.BRS(features, train_x, train_y, i+1)
 
             # Kind of tricky to obtain each training loss, we can't with fit so we need to loop each sample. Will leave it for now
             # cl_model.LARS(train_x, train_y, i+1, passthelosshere)
